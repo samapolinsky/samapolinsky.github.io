abb9003b2f1166c580d4ebb15b115dfef330a47b
samapolinsky <ybw5mp@virginia.edu>
2025-10-13 16:43:13 -0400

    resume not showing up

--- assets/js/script.js
+++ assets/js/script.js
@@ -141,13 +141,13 @@ const pages = document.querySelectorAll("[data-page]");
 console.log(navigationLinks.length, pages.length);
 
 // add event to all nav link
-for (let i = 0; i < navigationLinks.length; i++) {
-    navigationLinks[i].addEventListener("click", function () {
+for (let j = 0; j < navigationLinks.length; j++) {
+    navigationLinks[j].addEventListener("click", function () {
 
         for (let i = 0; i < pages.length; i++) {
             if (this.innerHTML.toLowerCase() === pages[i].dataset.page) {
                 pages[i].classList.add("active");
-                navigationLinks[i].classList.add("active");
+                navigationLinks[j].classList.add("active");
                 window.scrollTo(0, 0);
             } else {
                 pages[i].classList.remove("active");

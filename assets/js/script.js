12182fee31118e3b72ab867b220167b7743e7433
samapolinsky <ybw5mp@virginia.edu>
2025-10-13 14:18:35 -0400

    console log

--- assets/js/script.js
+++ assets/js/script.js
@@ -140,6 +140,8 @@ for (let i = 0; i < formInputs.length; i++) {
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
 
+console.log(navigationLinks.length, pages.length);
+
 // add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
     navigationLinks[i].addEventListener("click", function () {
